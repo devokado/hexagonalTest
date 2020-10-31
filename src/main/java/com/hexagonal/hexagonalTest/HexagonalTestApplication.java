475193d251bf50930654e31457bf8ccf46e9ec1d
Nejatians <nejatian@outlook.com>
2020-10-31 18:16:49 +0330

    initial new files

---
+++ src/main/java/com/hexagonal/hexagonalTest/HexagonalTestApplication.java
@@ -0,0 +1,13 @@
+package com.hexagonal.hexagonalTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hexagonal/hexagonalTest/HexagonalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexagonal.hexagonalTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
